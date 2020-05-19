46b81f0c819866a9dcfa35f41603580805e46acd
Andrii <64638375+andriise@users.noreply.github.com>
2020-05-19 21:17:23 +0300

    Add files via upload

--- www/js/main.js
+++ www/js/main.js
@@ -1,17 +1,44 @@
 
-var btn = document.getElementById("btn");
-console.log(window.innerWidth);
+ var elements = Array.from(document.getElementsByClassName("bg-light"));
+ var elements2 = Array.from(document.getElementsByClassName("navbar-light"));
+ var elements3 = Array.from(document.getElementsByClassName("text-light"));
 
-function btnclk() {
-    var elements = document.getElementsByClassName("bg-light");
-    if (elements[0]) {
-        
-                    elements[0].className = "bg-dark";
-        
-                    replaceClass();
-
-};
-
-btn.onclick = btnclk;
-
+function toggle() {
+  for (let i = 0; i < elements.length; i++) {
+      let el = elements[i];
+      if(el.classList.contains('bg-light')){
+          el.classList.add('bg-dark');
+          el.classList.remove('bg-light');
+      } else {
+          el.classList.add('bg-light');
+          el.classList.remove('bg-dark');
+      }
+  }
 }
+
+function toggle2() {
+  for (let i = 0; i < elements2.length; i++) {
+      let el2 = elements2[i];
+      if(el2.classList.contains('navbar-light')){
+          el2.classList.add('navbar-dark');
+          el2.classList.remove('navbar-light');
+      } else {
+          el2.classList.add('navbar-light');
+          el2.classList.remove('navbar-dark');
+      }
+  } 
+}
+
+function toggle3() {
+  for (let i = 0; i < elements3.length; i++) {
+      let el3 = elements3[i];
+      if(el3.classList.contains('text-light')){
+          el3.classList.add('text-dark');
+          el3.classList.remove('text-light');
+      } else {
+          el3.classList.add('text-light');
+          el3.classList.remove('text-dark');
+      }
+  } 
+}
+
